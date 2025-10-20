0627179128928024ae38d59970ed1f9aa2d08c0e
envilogger <a.kudryavcev@gmail.com>
2025-10-20 18:49:55 +0300

    [Sprint 2][Task 4] Refactor email validation in Email struct to use validator crate

--- auth-service/src/domain/email.rs
+++ auth-service/src/domain/email.rs
@@ -1,12 +1,14 @@
+use validator::validate_email;
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
 pub struct Email(String);
 
 impl Email {
     pub fn parse(s: String) -> Result<Email, String> {
-        if s.contains('@') {
+        if validate_email(&s) {
             Ok(Email(s))
         } else {
-            Err(format!("{} is not a valid email", s))
+            Err(format!("{} is not a valid email", &s))
         }
     }
 }
